e412bbb10ff1ede46af07afc6f092e65184659e7
danielbsn1 <jb375835@icloud.com>
2025-12-09 20:55:08 -0300

    Ajustes no banner e cards de brinquedos - imagens maiores no banner e cards menores

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function enviarWhatsApp(nomeProduto, referencia) {
     // SUBSTITUA PELO SEU NÚMERO DE WHATSAPP (com código do país, sem + ou espaços)
     // Exemplo: 5511999999999 (55 = Brasil, 11 = DDD, 999999999 = número)
-    const numeroWhatsApp = '5511999999999';
+    const numeroWhatsApp = '5533998288837';
     
     // Mensagem personalizada que será enviada
     const mensagem = `Olá! Gostaria de saber mais sobre o produto:\n\n*${nomeProduto}*\nReferência: ${referencia}\n\nPoderia me enviar mais informações?`;
